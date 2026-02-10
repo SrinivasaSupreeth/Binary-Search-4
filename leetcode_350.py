5afc2dca1c99214421dd82bbff0a2d48b5a1a15f
Y SrinivasaSupreeth <srinivasayenisetty@gmail.com>
2026-02-10 09:46:51 +0530

    My_sols

---
+++ leetcode_350.py
@@ -0,0 +1,11 @@
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        freq = Counter(nums1)
+        result = []
+
+        for num in nums2:
+            if freq[num] > 0:
+                result.append(num)
+                freq[num] -= 1
+
+        return result
---
+++ leetcode_4.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+
+        n, m = len(nums1), len(nums2)
+        left, right = 0, n
+
+        while left <= right:
+            cut1 = (left + right) // 2
+            cut2 = (n + m + 1) // 2 - cut1
+
+            l1 = float('-inf') if cut1 == 0 else nums1[cut1 - 1]
+            r1 = float('inf') if cut1 == n else nums1[cut1]
+
+            l2 = float('-inf') if cut2 == 0 else nums2[cut2 - 1]
+            r2 = float('inf') if cut2 == m else nums2[cut2]
+
+            if l1 <= r2 and l2 <= r1:
+                if (n + m) % 2 == 0:
+                    return (max(l1, l2) + min(r1, r2)) / 2
+                else:
+                    return max(l1, l2)
+
+            elif l1 > r2:
+                right = cut1 - 1
+            else:
+                left = cut1 + 1
+        
